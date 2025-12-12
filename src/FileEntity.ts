1ff348d88507006d83df696b8dfa23e6b715982b
Alfian Andi Nugraha <me@alfianandi.dev>
2025-12-12 15:34:54 +0700

    refactor: Add FileEntityParams type and refactor FileEntity

--- src/FileEntity.ts
+++ src/FileEntity.ts
@@ -1,24 +1,17 @@
+import type { FileEntityParams } from "./index.types";
+
 class FileEntity {
-	constructor(
-		/**
-		 * @description Name of file
-		 */
-		public name: string,
-		/**
-		 * @description Size in types
-		 */
-		public size: number,
-		/**
-		 * @description The resource location, it can be Blob or URL path
-		 */
-		public uri: string,
-		/**
-		 * @description The mime type of the file
-		 * @example "image/png"
-		 * @example "video/mp4"
-		 */
-		public type: string,
-	) {}
+	name: string;
+	size: number | undefined;
+	uri: string | undefined;
+	type: string;
+
+	constructor(params: FileEntityParams) {
+		this.name = params.name;
+		this.size = params.size;
+		this.uri = params.uri;
+		this.type = params.type;
+	}
 }
 
 export default FileEntity;
---
+++ src/index.types.ts
@@ -0,0 +1,23 @@
+export type FileEntityParams = {
+	/**
+	 * @description Name of file
+	 */
+	name: string;
+
+	/**
+	 * @description Size in types
+	 */
+	size?: number;
+
+	/**
+	 * @description The resource location, it can be Blob or URL path
+	 */
+	uri?: string;
+
+	/**
+	 * @description The mime type of the file
+	 * @example "image/png"
+	 * @example "video/mp4"
+	 */
+	type: string;
+};
